c19ba1a0b9a08b10bfc0db49d2a5673e2943666b
Ryan Hart <ryan@makenotion.com>
2022-01-15 09:55:56 -0800

    Basic working socket server, logging each message.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,30 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { io } from 'socket.io-client'
 
 export default function Home() {
+	useEffect((): any => {
+		console.log('setting up socket...')
+		const socket = io({
+			path: "/api/socketio"
+		})
+
+		// TODO: Figure out why this needs to be handled specifically?
+		socket.on('connect', () => {
+			console.log('Socket connected.', socket.id)
+		})
+
+		socket.onAny((eventName, data) => {
+			if (eventName === 'message') {
+				console.log('Message: ', data)
+			} else if (eventName === 'block-created') {
+				console.log('Block Created: ', data)
+			}
+		})
+
+		if (socket) return () => socket.disconnect()
+	}, [])
+
   return (
     <ul>
       <li>
--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,46 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import * as http from 'http'
+// import { parse } from 'url'
 import next from 'next'
+import express, { Express, Request, Response } from 'express'
+import * as socketio from 'socket.io'
+import faker from 'faker'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextApp = next({ dev })
+const nextHandler = nextApp.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+nextApp.prepare().then(async () => {
+	const app: Express = express()
+	const server: http.Server = http.createServer(app)
+	const io: socketio.Server = new socketio.Server(server, {
+		path: "/api/socketio"
+	})
+	// io.attach(server)
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+	app.get('/hello', async (_: Request, res: Response) => {
+		res.send('Hello, world!')
+	})
+
+	io.on('connection', (socket: socketio.Socket) => {
+		console.log('connected')
+		socket.emit('message', 'Welcome to socket!')
+		socket.on('disconnect', () => {
+			console.log('disconnected')
+		})
+
+		setInterval(() => {
+			// Send random events with random data
+			socket.emit('block-created', faker.lorem.sentence())
+		}, 1000)
+	})
+
+	app.all('*', async (req: Request, res: Response) => {
+		void nextHandler(req, res)
+	})
+
+	server.listen(port, () => {
+		console.log(`Listening on port ${port}...`)
+	})
+
 })
